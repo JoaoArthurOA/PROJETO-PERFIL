f4276842b32740a37f92499b32259cf10049e6b5
João Arthur <101645963+JoaoArthurOA@users.noreply.github.com>
2023-05-11 09:26:40 -0300

    Add files via upload

---
+++ gamer1/script.js
@@ -0,0 +1,7 @@
+alert("Olá Seja Bem-vindo")
+
+let p1=prompt("Digite Seu NOME");
+let p2=prompt("Digite um PODER que gostaria de ter");
+let p3=prompt("Digite um LUGAR que gostaria de estar");
+let msg= document.getElementById('msg')
+msg.innerHTML = <p>Olá(p1) o seu poder é a/o $(p2) você vai enfrentar o/a $(p3) no/em $(p4)</p>
